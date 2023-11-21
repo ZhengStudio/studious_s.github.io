00bcacdf463d64df2e8d695c5195df1316d737a9
zw0095935 <zhengbinyu@hihonor.com>
2023-11-21 16:21:10 +0800

    进入页设计

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div id="tree-directory"></div>
+    <div id="person-card"></div>
+    <div id="info-bg">
+      <main id="info-box">
+      
+      </main> 
+    </div>
+     
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+#info-bg{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+}
+#info-box{
+  background-color: aqua;
+  min-height: 100%;
+  min-width: 648px;
+  width: 50%;
+  margin: 0 auto;
+}
+#tree-directory{
+  background-color: aquamarine;
+}
+#person-card{
+  background-color: antiquewhite;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,68 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
-  <main>
-    <TheWelcome />
+  <main id="paopao-box">
+    <div class="text-box" v-for="item in paopaoNameList"><div class="text">{{item}}</div></div>
   </main>
 </template>
+
+
+<script setup>
+import { ref } from 'vue';
+let paopaoNameList = ref(["知识","游戏","日常","笔记","POWER"])
+</script>
+
+<style scoped lang="scss">
+#paopao-box{
+  display: flex;
+  transform: translate(50%,50%);
+  left: -50%;
+  top: -50%;
+  justify-content: space-around;
+    .text-box{
+      border-radius: 25%;
+      width: 88px;
+      height: 88px;
+      text-align: center;
+      line-height: 88px;
+      background: linear-gradient(45deg,blue 0%,pink 50%);
+      margin: 0 27px 0 8px;
+      animation: roll 5s linear infinite;
+      box-shadow: 0 0 5px wheat;
+      &:hover{
+        cursor: pointer;
+        animation: changeShape 1s forwards;
+      }
+      .text{
+        animation: roll 5s linear infinite;
+        animation-direction: reverse;
+        &:hover{
+          cursor: pointer;
+          animation: changeShape 1s;
+          animation-direction: reverse;
+        }
+      }
+    }
+}
+
+@keyframes roll {
+  0%{
+    transform: rotateZ(0);
+  }
+  100%{
+    transform: rotateZ(360deg);
+  }
+}
+@keyframes changeShape {
+  0%{
+    transform: rotateZ(0);
+    border-radius: 25%;
+  }
+  100%{
+    transform: rotateZ(1080deg);
+    border-radius: 50%;
+  }
+}
+
+
+</style>
